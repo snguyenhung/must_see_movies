48e9ef0903ee595b8d7a63c8c1ed56151251f5cb
anonymous anonymous <snguyenhung@gmail.com>
2021-11-16 04:29:48 +0000

    Whitelist columns in Interact for ActiveAdmin

--- app/admin/interacts.rb
+++ app/admin/interacts.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Interact do
 
+ permit_params :user_id, :rating, :review, :movie_id, :bookmark
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
