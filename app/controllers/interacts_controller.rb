edeebffafa8064ea8d2e3f04977a4c4a2cbba976
anonymous anonymous <snguyenhung@gmail.com>
2021-11-16 04:30:10 +0000

    Add forms for new collection objects on show pages

--- app/controllers/interacts_controller.rb
+++ app/controllers/interacts_controller.rb
@@ -24,7 +24,12 @@ class InteractsController < ApplicationController
     @interact = Interact.new(interact_params)
 
     if @interact.save
-      redirect_to @interact, notice: 'Interact was successfully created.'
+      message = 'Interact was successfully created.'
+      if Rails.application.routes.recognize_path(request.referrer)[:controller] != Rails.application.routes.recognize_path(request.path)[:controller]
+        redirect_back fallback_location: request.referrer, notice: message
+      else
+        redirect_to @interact, notice: message
+      end
     else
       render :new
     end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,7 @@ class UsersController < ApplicationController
 
   # GET /users/1
   def show
+    @interact = Interact.new
   end
 
   # GET /users/new
